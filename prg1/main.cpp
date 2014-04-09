28dd6157d5e2c8f7ed8ae1bea7a8c7bd7a4cb751
Kevin Colindres <kevin_hernandez@unitec.edu>
2014-04-09 16:17:14 -0500

    Periodo 2 2014

---
+++ prg1/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int n1, n2, suma;
+    cout << "Ingresar numero #1: ";
+    cin >> n1;
+
+    cout << "Ingresar numero #2: ";
+    cin >> n2;
+
+    suma = n1 + n2;
+
+    cout << "Suma de los numeros es: "<< suma;
+
+
+    return 0;
+}
